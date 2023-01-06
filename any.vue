f1147bd7ab027defae92a2fad36e956c8b40ecf4
lizhihua <275091674@qq.com>
2023-01-06 13:57:42 +0800

    feat: update

--- any.vue
+++ any.vue
@@ -9,12 +9,6 @@ import { ref } from 'vue'
 // 同步状态
 const title = ref('')
 
-// DOM实例创建后
-import { onMounted } from 'vue'
-onMounted(() => {
-
-})
-
 // 异步状态
 http.request('/').then((result) => { // 可能等待 3000ms 后，返回 result
   title.value = result
---
+++ state.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    {{title}}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// 同步状态
+const title = ref('')
+
+// 组件实例创建后
+import { onCreated } from 'vue'
+onCreated(() => {
+  title.value = 'onCreated'
+})
+
+// DOM实例创建后
+import { onMounted } from 'vue'
+onMounted(() => {
+  title.value = 'onMounted'
+})
+
+// 异步状态
+// 不确定的返回时机。可能等待 3000ms 后，也可能在 onCreated 和 onMounted 之间
+http.request('/').then((result) => {
+  title.value = result
+})
+</script>
